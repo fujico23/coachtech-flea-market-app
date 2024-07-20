44f6f4a4808e5b57671a032d0487cb1ea18c0fc0
fujico23 <k03c347b@yahoo.co.jp>
2024-07-20 16:28:06 +0900

    CircleCI:CD動作確認

---
+++ scripts/before_install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+rm -rf /home/ec2-user/coachtech-flea-market-app-main/src/.editorconfig
+rm -rf /home/ec2-user/coachtech-flea-market-app-main/src/.env.example
+rm -rf /home/ec2-user/coachtech-flea-market-app-main/src/.env.testing
--- scripts/clean.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-rm -rf /home/ec2-user/coachtech-flea-market-app-main/src/.editorconfig
