787f55ed563f5a9d198a4a8a2bba8ade6e5483d9
fujico23 <k03c347b@yahoo.co.jp>
2024-07-20 09:34:06 +0900

    CircleCI:CD動作確認

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,6 +3,9 @@
 set -eux
 
 #cd /var/www/grfl/src
-cd /home/ec2-user/coachtech-flea-market-app-main/src
-php artisan migrate --force
-php artisan config:cache
+cd /home/ec2-user/coachtech-flea-market-app-main
+docker-compose exec app php artisan migrate --force
+docker-compose exec app php config:cache
+#php artisan migrate --force
+#php artisan config:cache
+#coachtech-flea-market-app-main-php-1
